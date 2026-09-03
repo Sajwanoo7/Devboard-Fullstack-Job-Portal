fab291fb886e22f6d4d3e92b80e0fa9cbfb45069
Sajwanoo7 <168546472+Sajwanoo7@users.noreply.github.com>
2026-09-03 16:28:22 +0530

    Add files via upload

---
+++ backend/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const connectDB = require('./db');
+const authRoutes = require('./routes/auth');
+const auth = require('./middleware/auth');
+const jobRoutes = require('./routes/jobs');
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', authRoutes); // option 1 qadd it here
+app.use('/api/jobs', jobRoutes);
+app.get('/', (req, res) => {
+    res.send('Jobs portal is up!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
